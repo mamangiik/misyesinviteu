b04d2e2128de195b08d629fc5a286507b5828966
mamangiik <mmg.is.yes@gmail.com>
2025-07-05 10:58:59 +0700

    chore: bersihkan struktur folder, pindah ke catalog/

--- fix-static-links.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "🔍 Replace href link..."
-find ./ -type f -name "*.html" -exec sed -i 's|href="http://demo-undangan-|href="/catalog/demo-undangan-|g' {} +
-find ./ -type f -name "*.html" -exec sed -i 's|href="demo-undangan-|href="/catalog/demo-undangan-|g' {} +
-echo "✅ Semua link sudah diawali /catalog/"
-
